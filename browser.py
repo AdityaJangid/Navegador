a38efcf4e4657b9571d0a0401b14b96c40dfb1fd
AdityaJangid <eradityajangid@gmail.com>
2017-03-20 18:18:56 +0530

    created navaigation pannel

--- browser.py
+++ browser.py
@@ -8,17 +8,49 @@ class  Browser(gtk.Window):
         # created our main window
         self.set_default_size(int(b)-100, int(c)-100)
 
+        self.navigation = gtk.HBox()
+
         #create back, forward, refresh, and search pannel
         #back button
         self.back = gtk.ToolButton(gtk.STOCK_GO_BACK)
         self.frwd = gtk.ToolButton(gtk.STOCK_GO_FORWARD)
+        self.refresh = gtk.ToolButton(gtk.STOCK_REFRESH)
         self.address_bar = gtk.Entry()
+
+
+       # #what to do with buttons
+#        self.back.connect("clicked" , self.go_back)
+        self.frwd.connect("clicked" , self.go_frwd)
+#        self.refresh.connect("clicked" , self.refresh_page)
+#        self.address_bar.connect("activate" , self.load_page)
+#
+#    def go_back(self, arg1):
+#        """TODO: Docstring for go_back.
+#        """
+#        print("test of button for go back")
+# 
+        def go_frwd(self):
+            print("test of button for go back")
+# 
+#    def refresh_page(self, arg1):
+#        """TODO: Docstring for go_back.
+#        """
+#        print("test of button for go back")
+# 
+#    def load_page(self, arg1):
+#        """TODO: Docstring for go_back.
+#        """
+#        print("test of button for go back")
+# 
+
+
 
 
         fixed = gtk.Fixed()
         fixed.put(self.back, 0,0)
-        fixed.put(self.frwd, 30,0)
-        fixed.put(self.address_bar, )
+        fixed.put(self.frwd, 33,0)
+        fixed.put(self.refresh, 60,0)
+        fixed.put(self.address_bar,110,000 )
 
 
 
@@ -33,3 +65,5 @@ class  Browser(gtk.Window):
 
 Browser()
 gtk.main()
+
+
